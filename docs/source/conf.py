5d2f1f5aa088ffe8fa04b2f9d537531ab429681a
grace227 <37392904+grace227@users.noreply.github.com>
2022-12-28 15:27:07 -0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ intersphinx_mapping = {
 intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
-autodoc_mock_imports = ['epics', 'matplotlib', 'numpy', 'scipy', 'cv2', 'pandas']
+autodoc_mock_imports = ['epics', 'matplotlib', 'numpy', 'scipy', 'cv2', 'pandas', 'Transform']
 
 # -- Options for HTML output
 

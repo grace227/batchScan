aa2bc83ed68034eb423d64b423355a4833b5eb89
grace227 <37392904+grace227@users.noreply.github.com>
2022-12-28 11:10:15 -0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,10 +1,14 @@
 # Configuration file for the Sphinx documentation builder.
+import os, sys
+import sphinx_rtd_theme
+sys.path.insert(0, os.path.abspath('../../source'))  
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'BatchScan'
+aps = 'Argonne National Laboratory'
+copyright = '2020 - 2023, ' + aps
+
 
 release = '0.1'
 version = '0.1.0'
@@ -30,6 +34,16 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+html_theme_options = {
+    'style_nav_header_background': '#4f8fb8ff',
+    'collapse_navigation': False,
+    'logo_only': True,
+}
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = project + 'doc'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'

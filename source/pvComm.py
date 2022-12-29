f9e1936bc93d5010dfcfd1f444f6ad071eebd2a8
grace227 <37392904+grace227@users.noreply.github.com>
2022-12-29 15:33:12 -0800

    Update pvComm.py

--- source/pvComm.py
+++ source/pvComm.py
@@ -45,7 +45,8 @@ class pvComm():
         self.logfid.flush()
     
     def getDir(self):
-        """The function gets the current working directory from PV ::saveData_fileSystem
+        """The function gets the current working directory from a PV::
+            9idbBNP:saveData_fileSystem
         """
         fs = self.pvs['filesys'].pv.value
         fs = fs.replace('//micdata/data1', '/mnt/micdata1')

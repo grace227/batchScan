98729fb97bed7b038b6bebf4dbb332265720ee82
grace227 <37392904+grace227@users.noreply.github.com>
2022-12-30 10:18:13 -0800

    Update pvComm.py

--- source/pvComm.py
+++ source/pvComm.py
@@ -199,130 +199,130 @@ class pvComm():
         self.logger('%s: Move BDA to block position at: %.3f\n'%(t, bda_pos))
         self.pvs['BDA_pos'].put_callback(bda_pos)
         
-    def openBeamBDA(self, BDA):
-        """The function move BDA to its IN positon.
-        Involved PV ::
-            9idbTAU:UA:UX:RqsPos
+#     def openBeamBDA(self, BDA):
+#         """The function move BDA to its IN positon.
+#         Involved PV ::
+#             9idbTAU:UA:UX:RqsPos
             
-        Parameters
-        ----------
-        BDA : float
-            Motor position of BDA when it is in the beam path 
-        """
-        self.logger('%s: Move BDA to open position at: %.3f\n'%(getCurrentTime(), BDA))
-        self.pvs['BDA_pos'].put_callback(BDA)
+#         Parameters
+#         ----------
+#         BDA : float
+#             Motor position of BDA when it is in the beam path 
+#         """
+#         self.logger('%s: Move BDA to open position at: %.3f\n'%(getCurrentTime(), BDA))
+#         self.pvs['BDA_pos'].put_callback(BDA)
     
-    def changeXYcombinedMode(self):     
-        """The function change the x- and y- motor combined motion (coarse + piezo).
-        Involved PV ::
-            x motor mode --> 9idbTAU:SM:Ps:xMotionChoice.VAL = 0
-            y motor mode --> 9idbTAU:SY:Ps:yMotionChoice.VAL = 0
-        """
-        self.logger('%s; Changing XY scan mode to combined motion\n'%(getCurrentTime()))
-        self.pvs['x_motorMode'].pv.put(0)
-        self.pvs['y_motorMode'].pv.put(0)
+#     def changeXYcombinedMode(self):     
+#         """The function change the x- and y- motor combined motion (coarse + piezo).
+#         Involved PV ::
+#             x motor mode --> 9idbTAU:SM:Ps:xMotionChoice.VAL = 0
+#             y motor mode --> 9idbTAU:SY:Ps:yMotionChoice.VAL = 0
+#         """
+#         self.logger('%s; Changing XY scan mode to combined motion\n'%(getCurrentTime()))
+#         self.pvs['x_motorMode'].pv.put(0)
+#         self.pvs['y_motorMode'].pv.put(0)
         
-    def changeXtoCombinedMode(self):    
-        """The function change the x-motor combined motion (coarse + piezo).
-        Involved PV ::
-            x motor mode --> 9idbTAU:SM:Ps:xMotionChoice.VAL = 0
-        """
-        self.logger('%s; Changing XY scan mode to combined motion\n'%(getCurrentTime()))
-        self.pvs['x_motorMode'].pv.put(0) 
+#     def changeXtoCombinedMode(self):    
+#         """The function change the x-motor combined motion (coarse + piezo).
+#         Involved PV ::
+#             x motor mode --> 9idbTAU:SM:Ps:xMotionChoice.VAL = 0
+#         """
+#         self.logger('%s; Changing XY scan mode to combined motion\n'%(getCurrentTime()))
+#         self.pvs['x_motorMode'].pv.put(0) 
         
-    def changeXtoPiezolMode(self):
-        """The function change the x-motor to piezo mode.
-        Involved PV ::
-            x motor mode --> 9idbTAU:SM:Ps:xMotionChoice.VAL = 2
-        """
-        self.logger('%s: Changing X scan mode to Piezo only\n'%(getCurrentTime()))
-        self.pvs['x_motorMode'].pv.put(2)
+#     def changeXtoPiezolMode(self):
+#         """The function change the x-motor to piezo mode.
+#         Involved PV ::
+#             x motor mode --> 9idbTAU:SM:Ps:xMotionChoice.VAL = 2
+#         """
+#         self.logger('%s: Changing X scan mode to Piezo only\n'%(getCurrentTime()))
+#         self.pvs['x_motorMode'].pv.put(2)
 
-    def setXYcenter(self):
-        """The function udpates the current x- and y- motor position as the center of a scan.
-        Involved PV ::
-            x-scan record --> 9idbBNP:scan1.P1CP = 9idbTAU:SM:PX:RqsPos
-            y-scan record --> 9idbBNP:scan2.P1CP = 9idbTAU:SY:PY:RqsPos
-        """
-        self.logger('%s: Update the current position as the center of'\
-                    'the scan.\n'%(getCurrentTime()))
-        x_rqs = self.pvs['x_center_Rqs'].pv.get()
-        y_rqs = self.pvs['y_center_Rqs'].pv.get()
-        self.pvs['x_updatecenter'].pv.put(round(x_rqs, 2))
-        self.pvs['y_updatecenter'].pv.put(round(y_rqs, 2))
-        self.logger('%s: X_center valute: %.2f \n'%(getCurrentTime(), x_rqs))
-        self.logger('%s: Y_center valute: %.2f \n'%(getCurrentTime(), y_rqs))
-        # self.pvs['x_setcenter'].pv.put(1)
-        # self.pvs['y_setcenter'].pv.put(1)
+#     def setXYcenter(self):
+#         """The function udpates the current x- and y- motor position as the center of a scan.
+#         Involved PV ::
+#             x-scan record --> 9idbBNP:scan1.P1CP = 9idbTAU:SM:PX:RqsPos
+#             y-scan record --> 9idbBNP:scan2.P1CP = 9idbTAU:SY:PY:RqsPos
+#         """
+#         self.logger('%s: Update the current position as the center of'\
+#                     'the scan.\n'%(getCurrentTime()))
+#         x_rqs = self.pvs['x_center_Rqs'].pv.get()
+#         y_rqs = self.pvs['y_center_Rqs'].pv.get()
+#         self.pvs['x_updatecenter'].pv.put(round(x_rqs, 2))
+#         self.pvs['y_updatecenter'].pv.put(round(y_rqs, 2))
+#         self.logger('%s: X_center valute: %.2f \n'%(getCurrentTime(), x_rqs))
+#         self.logger('%s: Y_center valute: %.2f \n'%(getCurrentTime(), y_rqs))
+#         # self.pvs['x_setcenter'].pv.put(1)
+#         # self.pvs['y_setcenter'].pv.put(1)
         
-    def motorReady_XZTP(self):
-        status = self.pvs['xztp_motor_ready'].pv.get(as_string=True)
-        ready = 1 if status == 'Ready' else 0
-        return ready
+#     def motorReady_XZTP(self):
+#         status = self.pvs['xztp_motor_ready'].pv.get(as_string=True)
+#         ready = 1 if status == 'Ready' else 0
+#         return ready
         
-    def sumMotorDiff(self, motorlist):
-        sum_diff = 0
-        for m in motorlist:
-            sum_diff += abs(self.pvs['%s_Rqs'%m[0]].pv.get() - self.pvs['%s_Act'%m[0]].pv.get())
-        return sum_diff
+#     def sumMotorDiff(self, motorlist):
+#         sum_diff = 0
+#         for m in motorlist:
+#             sum_diff += abs(self.pvs['%s_Rqs'%m[0]].pv.get() - self.pvs['%s_Act'%m[0]].pv.get())
+#         return sum_diff
     
     
-    def centerPiezoXY(self):
-        MAX_WAIT_TIME = 5  #sec
-        self.logger('%s: Centering piezoX and piezoY.\n'%(getCurrentTime()))
-        self.pvs['piezo_xCenter'].pv.put(1)
-        self.pvs['piezo_yCenter'].pv.put(1)
-        self.logger('%s: Piezo xcenter value: %.2f\n'%(getCurrentTime(), self.pvs['x_piezo_val'].pv.get()))
-        self.logger('%s: Piezo ycenter value: %.2f\n'%(getCurrentTime(), self.pvs['y_piezo_val'].pv.get()))
-        tin = time.time()
-        t_diff = 0
-        while (not self.motorReady_XZTP()) & (t_diff < MAX_WAIT_TIME):
-            self.logger('%s: Waiting for XZTPX to be ready.\n'%(getCurrentTime()))
-            t_diff = time.time() - tin
-            time.sleep(0.2)
+#     def centerPiezoXY(self):
+#         MAX_WAIT_TIME = 5  #sec
+#         self.logger('%s: Centering piezoX and piezoY.\n'%(getCurrentTime()))
+#         self.pvs['piezo_xCenter'].pv.put(1)
+#         self.pvs['piezo_yCenter'].pv.put(1)
+#         self.logger('%s: Piezo xcenter value: %.2f\n'%(getCurrentTime(), self.pvs['x_piezo_val'].pv.get()))
+#         self.logger('%s: Piezo ycenter value: %.2f\n'%(getCurrentTime(), self.pvs['y_piezo_val'].pv.get()))
+#         tin = time.time()
+#         t_diff = 0
+#         while (not self.motorReady_XZTP()) & (t_diff < MAX_WAIT_TIME):
+#             self.logger('%s: Waiting for XZTPX to be ready.\n'%(getCurrentTime()))
+#             t_diff = time.time() - tin
+#             time.sleep(0.2)
         
-        return self.motorReady_XZTP()
+#         return self.motorReady_XZTP()
 
     
-    def assignPosValToPVs(self, pvstr, pvval):
-        for s_, v_ in zip(pvstr, pvval):
-            self.pvs[s_].pv.put(v_)
-            self.logger('%s: Change %s to %.3f\n' % (getCurrentTime(), s_, v_))
+#     def assignPosValToPVs(self, pvstr, pvval):
+#         for s_, v_ in zip(pvstr, pvval):
+#             self.pvs[s_].pv.put(v_)
+#             self.logger('%s: Change %s to %.3f\n' % (getCurrentTime(), s_, v_))
             
-    def assignSinglePV(self, pvstr, pvval):
-        self.pvs[pvstr].pv.put(pvval)
-        self.logger('%s: Change %s to %.3f\n' % (getCurrentTime(), pvstr, pvval))
+#     def assignSinglePV(self, pvstr, pvval):
+#         self.pvs[pvstr].pv.put(pvval)
+#         self.logger('%s: Change %s to %.3f\n' % (getCurrentTime(), pvstr, pvval))
             
-    def writeScanInit(self, mode, smpinfo, scandic):
-        next_sc = self.nextScanName()
-        self.logger('%s Initiating scan %s %s\n'%('#'*20, next_sc, '#'*20))
-        self.logger('Sample info: %s\n'% smpinfo)
-        self.logger('%s: Setting up scan using %s mode.\n'%(getCurrentTime(), mode))
-        self.logger('%s: %s'%(getCurrentTime(), scandic))
-        self.logger('\n\n')
+#     def writeScanInit(self, mode, smpinfo, scandic):
+#         next_sc = self.nextScanName()
+#         self.logger('%s Initiating scan %s %s\n'%('#'*20, next_sc, '#'*20))
+#         self.logger('Sample info: %s\n'% smpinfo)
+#         self.logger('%s: Setting up scan using %s mode.\n'%(getCurrentTime(), mode))
+#         self.logger('%s: %s'%(getCurrentTime(), scandic))
+#         self.logger('\n\n')
         
-    def motorReady(self, l, mt):
-        self.logger('%s: Checking whether motors are ready.\n'%(getCurrentTime()))
-        actpv = self.pvs['%s_Act'%l].pv
-        rqspv = self.pvs['%s_Rqs'%l].pv
-        self.pvs['%s_Act'%l].motorReady(rqspv, mt)
+#     def motorReady(self, l, mt):
+#         self.logger('%s: Checking whether motors are ready.\n'%(getCurrentTime()))
+#         actpv = self.pvs['%s_Act'%l].pv
+#         rqspv = self.pvs['%s_Rqs'%l].pv
+#         self.pvs['%s_Act'%l].motorReady(rqspv, mt)
         
-        if self.pvs['%s_Act'%l].motor_ready:
-            self.logger('%s: %s motor is in position with value'\
-                            '%.2f\n'%(getCurrentTime(), l, actpv.value))
-            return 1
-        else:
-            self.logger('%s: %s motor not in position, current: %.2f,'\
-                            ' request: %.2f\n'%(getCurrentTime(), l, actpv.value, rqspv.value))
-            return 0
+#         if self.pvs['%s_Act'%l].motor_ready:
+#             self.logger('%s: %s motor is in position with value'\
+#                             '%.2f\n'%(getCurrentTime(), l, actpv.value))
+#             return 1
+#         else:
+#             self.logger('%s: %s motor not in position, current: %.2f,'\
+#                             ' request: %.2f\n'%(getCurrentTime(), l, actpv.value, rqspv.value))
+#             return 0
     
-    def nextScanName(self):
-        return '%s%s.mda'%(self.pvs['basename'].pv.value, 
-                           str(self.pvs['nextsc'].pv.value).zfill(4))
+#     def nextScanName(self):
+#         return '%s%s.mda'%(self.pvs['basename'].pv.value, 
+#                            str(self.pvs['nextsc'].pv.value).zfill(4))
     
-    def getXYZcenter(self):
-        return [np.round(self.pvs[i].pv.value, 2) for i in ['x_center_Act', \
-                'y_center_Act', 'z_value_Act']]
+#     def getXYZcenter(self):
+#         return [np.round(self.pvs[i].pv.value, 2) for i in ['x_center_Act', \
+#                 'y_center_Act', 'z_value_Act']]
         
 
     
